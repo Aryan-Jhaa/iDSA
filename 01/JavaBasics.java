b749daee2b2e088cfcd4b573fefab8c70e80ed50
aryan <aryanjha813@gmail.com>
2025-09-29 19:27:21 +0530

    First commit

---
+++ 01/JavaBasics.java
@@ -0,0 +1,24 @@
+public class JavaBasics {
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+
+
+        // Variables 
+        int a = 10;
+        System.out.println(a);
+
+        byte b = 8;
+        System.out.println(b);
+
+        char c = 'c';
+        System.out.println(c);
+
+        boolean var = false;
+        System.out.println(var);
+
+        float price = 10.5f;
+        System.out.println(price);
+
+    }
+}
+
--- Day 0.md
+++
@@ -1,6 +0,0 @@
-Starting DSA after rejection from Eli Lilly.
-
-Started with Java, doing the basics of a language for the nth time, this time properly and keeping track of my learning.
-
-1. Class name should be same as java file name.
-2. 
