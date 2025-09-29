2dbd0badd656887b259595e8ade7f720adacdf4d
aryan <aryanjha813@gmail.com>
2025-09-29 19:52:37 +0530

    Update sum

--- 01/Sum.java
+++ 01/Sum.java
@@ -1,12 +1,19 @@
+import java.util.*;
+
 public class Sum {
     public static void main(String args[]){
         
         // sum of two numbers
 
-        int a = 10;
-        int b = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the value of second number: ");
+        int b = sc.nextInt();
+        
         int sum = a+b;
 
-        System.out.println(sum);
+        System.out.print("Sum of the two numbers: " + sum);
     }
 }
